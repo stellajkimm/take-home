96961d7d397c288ef173fb72183d29e6b65b31bc
Stella Kim <stellajkimm@gmail.com>
2014-10-21 21:49:52 -0400

    moved static files under take-home instead of athletes

--- take-home/settings.py
+++ take-home/settings.py
@@ -109,7 +109,7 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/athletes'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
